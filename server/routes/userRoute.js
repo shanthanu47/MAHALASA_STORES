29058366aa43232f85f47d7f63208188ead66202
shanthanu47 <shanthanu.shenoy16756@sakec.ac.in>
2026-01-02 21:07:22 +0530

    reset

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { isAuth, login, logout, register, deleteAccount } from '../controllers/userController.js';
+import { isAuth, login, logout, register, deleteAccount, sendResetOtp, resetPassword, verifyOtp, googleAuth } from '../controllers/userController.js';
 import authUser from '../middlewares/authUser.js';
 
 const userRouter = express.Router();
@@ -9,5 +9,9 @@ userRouter.post('/login', login)
 userRouter.get('/is-auth', authUser, isAuth)
 userRouter.get('/logout', authUser, logout)
 userRouter.post('/delete-account', authUser, deleteAccount)
+userRouter.post('/send-reset-otp', sendResetOtp)
+userRouter.post('/verify-otp', verifyOtp)
+userRouter.post('/reset-password', resetPassword)
+userRouter.post('/google-auth', googleAuth)
 
 export default userRouter
